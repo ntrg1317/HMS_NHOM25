9110efca2adc7656144aa1c0c3612c85dbb13648
Vang <vang1910tn@gmail.com>
2023-12-26 14:31:18 +0700

    hoadon

---
+++ HMS_NHOM25/SuaTTTK.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace HMS_NHOM25
+{
+    partial class SuaTTTK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SuaTTTK));
+            this.grbTTTKCu = new System.Windows.Forms.GroupBox();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtTenDN = new System.Windows.Forms.TextBox();
+            this.labMK = new System.Windows.Forms.Label();
+            this.labTenDN = new System.Windows.Forms.Label();
+            this.labSuaTTTK = new System.Windows.Forms.Label();
+            this.ptbSuaTTTK = new System.Windows.Forms.PictureBox();
+            this.grbTTTKMoi = new System.Windows.Forms.GroupBox();
+            this.txtMKmoi = new System.Windows.Forms.TextBox();
+            this.txtTenDNmoi = new System.Windows.Forms.TextBox();
+            this.labMKmoi = new System.Windows.Forms.Label();
+            this.labTenDNmoi = new System.Windows.Forms.Label();
+            this.btnLuuTT = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.grbTTTKCu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSuaTTTK)).BeginInit();
+            this.grbTTTKMoi.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbTTTKCu
+            // 
+            this.grbTTTKCu.Controls.Add(this.txtMK);
+            this.grbTTTKCu.Controls.Add(this.txtTenDN);
+            this.grbTTTKCu.Controls.Add(this.labMK);
+            this.grbTTTKCu.Controls.Add(this.labTenDN);
+            this.grbTTTKCu.ForeColor = System.Drawing.Color.Red;
+            this.grbTTTKCu.Location = new System.Drawing.Point(71, 90);
+            this.grbTTTKCu.Name = "grbTTTKCu";
+            this.grbTTTKCu.Size = new System.Drawing.Size(400, 100);
+            this.grbTTTKCu.TabIndex = 0;
+            this.grbTTTKCu.TabStop = false;
+            this.grbTTTKCu.Text = "Thông tin tài khoản cũ";
+            // 
+            // txtMK
+            // 
+            this.txtMK.Location = new System.Drawing.Point(192, 62);
+            this.txtMK.Name = "txtMK";
+            this.txtMK.Size = new System.Drawing.Size(154, 22);
+            this.txtMK.TabIndex = 3;
+            // 
+            // txtTenDN
+            // 
+            this.txtTenDN.Location = new System.Drawing.Point(192, 31);
+            this.txtTenDN.Name = "txtTenDN";
+            this.txtTenDN.Size = new System.Drawing.Size(154, 22);
+            this.txtTenDN.TabIndex = 2;
+            // 
+            // labMK
+            // 
+            this.labMK.AutoSize = true;
+            this.labMK.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labMK.Location = new System.Drawing.Point(48, 65);
+            this.labMK.Name = "labMK";
+            this.labMK.Size = new System.Drawing.Size(61, 16);
+            this.labMK.TabIndex = 1;
+            this.labMK.Text = "Mật khẩu";
+            // 
+            // labTenDN
+            // 
+            this.labTenDN.AutoSize = true;
+            this.labTenDN.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labTenDN.Location = new System.Drawing.Point(48, 34);
+            this.labTenDN.Name = "labTenDN";
+            this.labTenDN.Size = new System.Drawing.Size(98, 16);
+            this.labTenDN.TabIndex = 0;
+            this.labTenDN.Text = "Tên đăng nhập";
+            // 
+            // labSuaTTTK
+            // 
+            this.labSuaTTTK.AutoSize = true;
+            this.labSuaTTTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labSuaTTTK.Location = new System.Drawing.Point(153, 30);
+            this.labSuaTTTK.Name = "labSuaTTTK";
+            this.labSuaTTTK.Size = new System.Drawing.Size(277, 29);
+            this.labSuaTTTK.TabIndex = 2;
+            this.labSuaTTTK.Text = "Sửa thông tin tài khoản";
+            // 
+            // ptbSuaTTTK
+            // 
+            this.ptbSuaTTTK.ErrorImage = ((System.Drawing.Image)(resources.GetObject("ptbSuaTTTK.ErrorImage")));
+            this.ptbSuaTTTK.Image = ((System.Drawing.Image)(resources.GetObject("ptbSuaTTTK.Image")));
+            this.ptbSuaTTTK.Location = new System.Drawing.Point(108, 21);
+            this.ptbSuaTTTK.Name = "ptbSuaTTTK";
+            this.ptbSuaTTTK.Size = new System.Drawing.Size(48, 43);
+            this.ptbSuaTTTK.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptbSuaTTTK.TabIndex = 3;
+            this.ptbSuaTTTK.TabStop = false;
+            this.ptbSuaTTTK.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // grbTTTKMoi
+            // 
+            this.grbTTTKMoi.Controls.Add(this.txtMKmoi);
+            this.grbTTTKMoi.Controls.Add(this.txtTenDNmoi);
+            this.grbTTTKMoi.Controls.Add(this.labMKmoi);
+            this.grbTTTKMoi.Controls.Add(this.labTenDNmoi);
+            this.grbTTTKMoi.ForeColor = System.Drawing.Color.Red;
+            this.grbTTTKMoi.Location = new System.Drawing.Point(71, 205);
+            this.grbTTTKMoi.Name = "grbTTTKMoi";
+            this.grbTTTKMoi.Size = new System.Drawing.Size(400, 100);
+            this.grbTTTKMoi.TabIndex = 4;
+            this.grbTTTKMoi.TabStop = false;
+            this.grbTTTKMoi.Text = "Thông tin tài khoản mới";
+            // 
+            // txtMKmoi
+            // 
+            this.txtMKmoi.Location = new System.Drawing.Point(192, 59);
+            this.txtMKmoi.Name = "txtMKmoi";
+            this.txtMKmoi.Size = new System.Drawing.Size(154, 22);
+            this.txtMKmoi.TabIndex = 3;
+            // 
+            // txtTenDNmoi
+            // 
+            this.txtTenDNmoi.Location = new System.Drawing.Point(192, 28);
+            this.txtTenDNmoi.Name = "txtTenDNmoi";
+            this.txtTenDNmoi.Size = new System.Drawing.Size(154, 22);
+            this.txtTenDNmoi.TabIndex = 2;
+            // 
+            // labMKmoi
+            // 
+            this.labMKmoi.AutoSize = true;
+            this.labMKmoi.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labMKmoi.Location = new System.Drawing.Point(48, 62);
+            this.labMKmoi.Name = "labMKmoi";
+            this.labMKmoi.Size = new System.Drawing.Size(86, 16);
+            this.labMKmoi.TabIndex = 1;
+            this.labMKmoi.Text = "Mật khẩu mới";
+            // 
+            // labTenDNmoi
+            // 
+            this.labTenDNmoi.AutoSize = true;
+            this.labTenDNmoi.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.labTenDNmoi.Location = new System.Drawing.Point(48, 31);
+            this.labTenDNmoi.Name = "labTenDNmoi";
+            this.labTenDNmoi.Size = new System.Drawing.Size(123, 16);
+            this.labTenDNmoi.TabIndex = 0;
+            this.labTenDNmoi.Text = "Tên đăng nhập mới";
+            // 
+            // btnLuuTT
+            // 
+            this.btnLuuTT.Image = ((System.Drawing.Image)(resources.GetObject("btnLuuTT.Image")));
+            this.btnLuuTT.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLuuTT.Location = new System.Drawing.Point(108, 325);
+            this.btnLuuTT.Name = "btnLuuTT";
+            this.btnLuuTT.Size = new System.Drawing.Size(128, 32);
+            this.btnLuuTT.TabIndex = 5;
+            this.btnLuuTT.Text = "Lưu thông tin";
+            this.btnLuuTT.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLuuTT.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(292, 325);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(128, 32);
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // SuaTTTK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 374);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLuuTT);
+            this.Controls.Add(this.grbTTTKMoi);
+            this.Controls.Add(this.ptbSuaTTTK);
+            this.Controls.Add(this.labSuaTTTK);
+            this.Controls.Add(this.grbTTTKCu);
+            this.Name = "SuaThongTinTaiKhoan";
+            this.Text = "SuaThongTinTaiKhoan";
+            this.grbTTTKCu.ResumeLayout(false);
+            this.grbTTTKCu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbSuaTTTK)).EndInit();
+            this.grbTTTKMoi.ResumeLayout(false);
+            this.grbTTTKMoi.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbTTTKCu;
+        private System.Windows.Forms.Label labSuaTTTK;
+        private System.Windows.Forms.PictureBox ptbSuaTTTK;
+        private System.Windows.Forms.Label labTenDN;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtTenDN;
+        private System.Windows.Forms.Label labMK;
+        private System.Windows.Forms.GroupBox grbTTTKMoi;
+        private System.Windows.Forms.TextBox txtMKmoi;
+        private System.Windows.Forms.TextBox txtTenDNmoi;
+        private System.Windows.Forms.Label labMKmoi;
+        private System.Windows.Forms.Label labTenDNmoi;
+        private System.Windows.Forms.Button btnLuuTT;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
