07c47e91bbb5c067edb0059625a65eaf07860a56
Admin <trangnt1317@gmail.com>
2023-12-30 09:58:20 +0700

    Chính size

--- HMS_NHOM25/Program.cs
+++ HMS_NHOM25/Program.cs
@@ -17,15 +17,6 @@ namespace HMS_NHOM25
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Application.Run(new login());
-            //Application.Run(new AddThuoc());
-            //Application.Run(new Doctors());
-            //Application.Run(new Admin());
-            //Application.Run(new NVYTe());
-            //Application.Run(new NVThuNgan());
-            //Application.Run(new AddDoctor());
-            //Application.Run(new DonThuoc());
-            //Application.Run(new AddDonThuoc());
             Application.Run(new AdminControl());
         }
     }
