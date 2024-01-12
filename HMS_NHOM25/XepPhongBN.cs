fba4a989ae96f2edc250087300fa2b6588bcdf51
Hue Pham <thuhue3503@gmail.com>
2024-01-12 23:26:10 +0700

    đổi nốt tên cột form xếp phòng

--- HMS_NHOM25/XepPhongBN.cs
+++ HMS_NHOM25/XepPhongBN.cs
@@ -44,18 +44,14 @@ namespace HMS_NHOM25
                     "AND bn.TrangThai = 1";
 
                 dgvTimBN.DataSource = benhNhan.Table(query);
+                dgvTimBN.Columns["MaBN"].HeaderText = "Mã bệnh nhân";
+                dgvTimBN.Columns["TenBN"].HeaderText = "Tên bệnh nhân";
+                dgvTimBN.Columns["GioiTinh"].HeaderText = "Giới Tính";
+                dgvTimBN.Columns["BenhTrang"].HeaderText = "Bệnh Trạng";
+                dgvTimBN.Columns["NgayVao"].HeaderText = "Ngày Vào";
+                dgvTimBN.Columns["DiaChi"].HeaderText = "Địa Chỉ";
+                dgvTimBN.Columns["SDT"].HeaderText = "Số Điện Thoại";
             }
-        }
-
-        private void XepPhongBN_Load(object sender, EventArgs e)
-        {
-            dgvTimBN.Columns["MaBN"].HeaderText = "Mã bệnh nhân";
-            dgvTimBN.Columns["TenBN"].HeaderText = "Tên bệnh nhân";
-            dgvTimBN.Columns["GioiTinh"].HeaderText = "Giới Tính";
-            dgvTimBN.Columns["BenhTrang"].HeaderText = "Bệnh Trạng";
-            dgvTimBN.Columns["NgayVao"].HeaderText = "Ngày Vào";
-            dgvTimBN.Columns["DiaChi"].HeaderText = "Địa Chỉ";
-            dgvTimBN.Columns["SDT"].HeaderText = "Số Điện Thoại";
         }
         private bool CheckTextBoxes()
         {
@@ -110,6 +106,17 @@ namespace HMS_NHOM25
         private void btnThoatAddDonThuoc_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void XepPhongBN_Load(object sender, EventArgs e)
+        {
+            dgvTimBN.Columns["MaBN"].HeaderText = "Mã bệnh nhân";
+            dgvTimBN.Columns["TenBN"].HeaderText = "Tên bệnh nhân";
+            dgvTimBN.Columns["GioiTinh"].HeaderText = "Giới Tính";
+            dgvTimBN.Columns["BenhTrang"].HeaderText = "Bệnh Trạng";
+            dgvTimBN.Columns["NgayVao"].HeaderText = "Ngày Vào";
+            dgvTimBN.Columns["DiaChi"].HeaderText = "Địa Chỉ";
+            dgvTimBN.Columns["SDT"].HeaderText = "Số Điện Thoại";
         }
     }
 }
